93284f6febb8c4053f5ca8180755f521887fa4f0
Bluesoul-dev <276431064@qq.com>
2019-11-13 23:54:23 -0800

    added a lab3exercise1.c file

---
+++ lab/lab3exercise1.c
@@ -0,0 +1,58 @@
+
+#include<stdio.h>
+#define MAXLINE 200
+ 
+int getline(char line[], int max);
+int strrindex(char source[], char searchfor[]);
+ 
+char pattern[] = "ould"; 
+ 
+int getline(char s[], int lim)
+{
+	int c, i;
+	
+	i = 0;
+	while(--lim > 0 && (c = getchar()) != EOF && c!= '\n')  
+		s[i++] = c;
+	if(c == '\n')
+		s[i++] = c;
+	s[i] = '\0';
+	return i;
+}
+ 
+int strrindex(char s[], char t[])  
+{
+	int i, j, k, r;  
+	for(i = 0; s[i] != '\0'; i++)
+	{
+		for(j = i, k = 0; t[k] != '\0' && s[j] == t[k]; j++, k++)
+			;
+		if(k > 0 && t[k] == '\0')
+			r = i;	
+	}
+	if(r >= 0)
+		return r;
+	else
+		return -1;
+}
+ 
+
+int main()	
+{
+	char line[MAXLINE];
+	int found = 0;	
+ 
+	while(getline(line, MAXLINE) > 0)  
+	{
+		if(strrindex(line, pattern) >= 0)
+		{
+			printf("%s", line);
+			printf("%d\n", strrindex(line, pattern));  
+			found ++;
+		}
+	}
+	printf("%d\n", strrindex(line, pattern));
+	return found;
+}
+
+
